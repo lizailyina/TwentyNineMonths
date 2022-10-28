ec0b232e4607612771718091e9dd216bd4dbfa1b
lizailyina <liza-ilina-13052005@mail.ru>
2022-10-28 03:37:47 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,18 +2,16 @@ var cards = document.getElementsByClassName("card-img-top");
 
 function handleClicks(i) {
     return function() {
-        console.log("clicked card " + i);
-        console.log(cards[i].src);
         if(cards[i].id == "1") {
-            cards[i].src = "./src/image2.jpg";
+            cards[i].src = "src/image2.jpg";
             cards[i].id = "2";
         }
         else if(cards[i].id == "2") {
-            cards[i].src = "./src/image3.jpg";
+            cards[i].src = "src/image3.jpg";
             cards[i].id = "3";
         }
         else {
-            cards[i].src = "./src/image1.jpg";
+            cards[i].src = "src/image1.jpg";
             cards[i].id = "1";
         }
     }
