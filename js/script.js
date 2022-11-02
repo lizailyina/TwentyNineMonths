299f87f56e22a319b6b5ea4556f6001a457f7231
lizailyina <liza-ilina-13052005@mail.ru>
2022-11-02 06:13:13 +0300

    heh -_-

--- js/script.js
+++ js/script.js
@@ -1,22 +1,24 @@
-var cards = document.getElementsByClassName("card-img-top");
+// var cards = document.getElementsByClassName("card-img-top");
 
-function handleClicks(i) {
-    return function() {
-        if(cards[i].id == "1") {
-            cards[i].src = "TwentyNineMonths/src/image2.jpg";
-            cards[i].id = "2";
-        }
-        else if(cards[i].id == "2") {
-            cards[i].src = "TwentyNineMonths/src/image3.jpg";
-            cards[i].id = "3";
-        }
-        else {
-            cards[i].src = "TwentyNineMonths/src/image1.jpg";
-            cards[i].id = "1";
-        }
-    }
-}
+// function handleClicks(i) {
+//     return function() {
+//         if(cards[i].id == "1") {
+//             cards[i].src = "TwentyNineMonths/src/image2.jpg";
+//             cards[i].id = "2";
+//         }
+//         else if(cards[i].id == "2") {
+//             cards[i].src = "TwentyNineMonths/src/image3.jpg";
+//             cards[i].id = "3";
+//         }
+//         else {
+//             cards[i].src = "TwentyNineMonths/src/image1.jpg";
+//             cards[i].id = "1";
+//         }
+//     }
+// }
 
-for(var i = 0; i < cards.length; ++i) {
-    cards[i].addEventListener('click', handleClicks(i));
-}
+// for(var i = 0; i < cards.length; ++i) {
+//     cards[i].addEventListener('click', handleClicks(i));
+// }
+
+// var d = getElementsByClassName("div");
